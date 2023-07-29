5b3e3a20667932745605ca2e8b5549f4a763a97e
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-07-29 23:27:45 +0200

    nodemailer err

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,24 +1,52 @@
+// const nodemailer = require("nodemailer");
+
+// const sendMail = async (options) => {
+//     const transporter = nodemailer.createTransport({
+//         service:"gmail",
+//         secure: true,
+//         host: process.env.SMPT_HOST,
+//         port: process.env.SMPT_PORT,
+//         service: process.env.SMPT_SERVICE,
+//         auth:{
+//             user: process.env.SMPT_MAIL,
+//             pass: process.env.SMPT_PASSWORD,
+//         },
+//     });
+
+//     const mailOptions = {
+//         from: process.env.SMPT_MAIL,
+//         to: options.email,
+//         subject: options.subject,
+//         text: options.message,
+//     };
+
+//     await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendMail;
+
 const nodemailer = require("nodemailer");
 
 const sendMail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMPT_HOST,
-        port: process.env.SMPT_PORT,
-        service: process.env.SMPT_SERVICE,
-        auth:{
-            user: process.env.SMPT_MAIL,
-            pass: process.env.SMPT_PASSWORD,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // Use "gmail" service directly here
+    secure: true,
+    host: process.env.SMPT_HOST,
+    port: process.env.SMPT_PORT,
+    auth: {
+      user: process.env.SMPT_MAIL,
+      pass: process.env.SMPT_PASSWORD,
+    },
+  });
 
-    const mailOptions = {
-        from: process.env.SMPT_MAIL,
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    };
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
 
-    await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendMail;
