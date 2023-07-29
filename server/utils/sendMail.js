acc8303b9153cb5f7799a32f5a28cd4834e315a4
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-07-29 23:48:39 +0200

    solve err

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,52 +1,36 @@
-// const nodemailer = require("nodemailer");
-
-// const sendMail = async (options) => {
-//     const transporter = nodemailer.createTransport({
-//         service:"gmail",
-//         secure: true,
-//         host: process.env.SMPT_HOST,
-//         port: process.env.SMPT_PORT,
-//         service: process.env.SMPT_SERVICE,
-//         auth:{
-//             user: process.env.SMPT_MAIL,
-//             pass: process.env.SMPT_PASSWORD,
-//         },
-//     });
-
-//     const mailOptions = {
-//         from: process.env.SMPT_MAIL,
-//         to: options.email,
-//         subject: options.subject,
-//         text: options.message,
-//     };
-
-//     await transporter.sendMail(mailOptions);
-// };
-
-// module.exports = sendMail;
 
 const nodemailer = require("nodemailer");
 
+// Create a reusable transporter object outside the function
+const transporter = nodemailer.createTransport({
+  host: process.env.SMPT_HOST,
+  port: process.env.SMPT_PORT,
+  service: process.env.SMPT_SERVICE,
+  auth: {
+    user: process.env.SMPT_MAIL,
+    pass: process.env.SMPT_PASSWORD,
+  },
+});
+
+// Wrap the sendMail function in a try-catch block to handle errors
 const sendMail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail", // Use "gmail" service directly here
-    secure: true,
-    host: process.env.SMPT_HOST,
-    port: process.env.SMPT_PORT,
-    auth: {
-      user: process.env.SMPT_MAIL,
-      pass: process.env.SMPT_PASSWORD,
-    },
-  });
+  try {
+    const mailOptions = {
+      from: process.env.SMPT_MAIL,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+    };
 
-  const mailOptions = {
-    from: process.env.SMPT_MAIL,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+    // Use async/await for sending the email
+    await transporter.sendMail(mailOptions);
 
-  await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully.");
+    return true; // Return true to indicate success if needed
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return false; // Return false to indicate failure if needed
+  }
 };
 
 module.exports = sendMail;
