f9e06192fb961c9973748331236409fd5df88672
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-08-03 14:58:54 +0200

    upgrading

--- server/app.js
+++ server/app.js
@@ -1,28 +1,20 @@
 const express = require("express");
-const ErrorHandler = require("./middleware/error");
-const app = express();
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const ErrorHandler = require("./middleware/error");
 
+const app = express();
+
+// Middleware
 app.use(
   cors({
-    origin: ["https://emarket-jet.vercel.app"],
+    origin: "https://emarket-jet.vercel.app",
     credentials: true,
   })
 );
 
-app.use(bodyParser.json({ limit: "50mb" }));
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-    limit: "50mb",
-    parameterLimit: 50000,
-  })
-);
-
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -31,30 +23,30 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   });
 }
 
-// import routes
-const user = require("./controller/user");
-const shop = require("./controller/shop");
-const product = require("./controller/product");
-const event = require("./controller/event");
-const coupon = require("./controller/coupounCode");
-const payment = require("./controller/payment");
-const order = require("./controller/order");
-const conversation = require("./controller/conversation");
-const message = require("./controller/message");
-const withdraw = require("./controller/withdraw");
+// Routes
+const userRoutes = require("./controller/user");
+const shopRoutes = require("./controller/shop");
+const productRoutes = require("./controller/product");
+const eventRoutes = require("./controller/event");
+const couponRoutes = require("./controller/coupounCode");
+const paymentRoutes = require("./controller/payment");
+const orderRoutes = require("./controller/order");
+const conversationRoutes = require("./controller/conversation");
+const messageRoutes = require("./controller/message");
+const withdrawRoutes = require("./controller/withdraw");
 
-app.use("/api/v2/user", user);
-app.use("/api/v2/conversation", conversation);
-app.use("/api/v2/message", message);
-app.use("/api/v2/order", order);
-app.use("/api/v2/shop", shop);
-app.use("/api/v2/product", product);
-app.use("/api/v2/event", event);
-app.use("/api/v2/coupon", coupon);
-app.use("/api/v2/payment", payment);
-app.use("/api/v2/withdraw", withdraw);
+app.use("/api/v2/user", userRoutes);
+app.use("/api/v2/conversation", conversationRoutes);
+app.use("/api/v2/message", messageRoutes);
+app.use("/api/v2/order", orderRoutes);
+app.use("/api/v2/shop", shopRoutes);
+app.use("/api/v2/product", productRoutes);
+app.use("/api/v2/event", eventRoutes);
+app.use("/api/v2/coupon", couponRoutes);
+app.use("/api/v2/payment", paymentRoutes);
+app.use("/api/v2/withdraw", withdrawRoutes);
 
-// it's for ErrorHandling
+// Error handling middleware
 app.use(ErrorHandler);
 
 module.exports = app;
