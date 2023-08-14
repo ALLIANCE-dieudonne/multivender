a94ea56bfb99f58538c23795bfb8ad1034788cee
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-08-15 00:44:59 +0200

    update

--- client/src/pages/EventsPage.jsx
+++ client/src/pages/EventsPage.jsx
@@ -15,7 +15,7 @@ const EventsPage = () => {
         <div>
           <Header activeHeading={4} />
 
-          <div className="w-[90%] flex justify-center flex-col flex-wrap">
+          <div className="w-[90%] ml-3">
             {allEvents.map((event) => (
               <EventCard key={event._id} active={true} data={event} />
             ))}{" "}
